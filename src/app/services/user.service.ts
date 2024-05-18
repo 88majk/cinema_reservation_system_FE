d14336b864c61fcce93a2c1161c5d8a198c0fb90
88majk <mikolaj19gr@gmail.com>
2024-05-18 22:45:09 +0200

    Connected user edit forms with backend endpoints.

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable, inject } from '@angular/core';
+import { UpdateUserData } from '../models/user-update-data';
 
 @Injectable({
   providedIn: 'root'
@@ -10,5 +11,13 @@ export class UserService {
 
   getUserData(email: string) {
     return this.http.get(`http://localhost:8080/users/${email}`);
+  }
+
+  updateUserData(updateData: UpdateUserData) {
+    return this.http.put(`http://localhost:8080/users/update/` + localStorage.getItem('user_token'), updateData);
+  }
+
+  updateUserPassword(passsword: string) {
+    return this.http.post(`http://localhost:8080/users/changePassword/` + localStorage.getItem('user_token'), passsword);
   }
 }
